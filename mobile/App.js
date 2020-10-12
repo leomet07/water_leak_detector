60a28643e120eaba0fe668daaa868c1e9ed491ba
leomet07 <leomet07@gmail.com>
2020-10-11 22:36:36 -0400

    Basic Retrival

--- mobile/App.js
+++ mobile/App.js
@@ -1,16 +1,57 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+
+import React, {Component} from "react";
+
 import { StyleSheet, Text, View } from "react-native";
+export default class App extends Component {
+   
+    constructor() {
+        super();
+        //save the scope so we can use this in a diff scope
+        
+        this.state = {
+            leaks : []
+        };
+    }
+    componentWillUnmount() {
+        
+    }
+    async componentDidMount() {
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>View Your Building's Water Leak Alerts Here!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+		console.log("Mounted");
+
+		let response = await fetch("https://waterleakbackend.herokuapp.com/api/db/get_leaks");
+
+		const json = await response.json();
+
+		console.log(JSON.stringify(json))
+        
+        this.setState({leaks : json});
+        //this.registerForPushNotifications()
+    }
+
+    
+
+    render() {
+		const leakItems = this.state.leaks.map((data) => {
+
+			let uri = data._id;
+			console.log(uri)
+			return (
+				<Text key={uri}>
+					{data.date}
+				</Text>
+			);
+		});
+
+
+        return (
+            <View style={styles.container}>
+               <Text>Hello</Text>
+			   <View>{leakItems}</View>
+            </View>
+        );
+    }
 }
-
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
