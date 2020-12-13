063de37454c98847b4c799b369df19787392f220
leomet07 <leomet07@gmail.com>
2020-12-12 20:05:22 -0500

    New Method of recieving tokens.

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,25 @@
-
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
-import { Notifications } from "expo";
+// import { Notifications } from "expo";
+import * as Notifications from "expo-notifications";
 import * as Permissions from "expo-permissions";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./Styles";
-import HomeScreen from "./Home"
-import LoginScreen from "./Login"
-import SettingsScreen from "./Settings"
-import * as Device from 'expo-device';
-import { AsyncStorage } from 'react-native';
-var ee = require('event-emitter');
+import HomeScreen from "./Home";
+import LoginScreen from "./Login";
+import SettingsScreen from "./Settings";
+import * as Device from "expo-device";
+import { AsyncStorage } from "react-native";
+
+var ee = require("event-emitter");
 
 if (__DEV__) {
-    console.log('Development');
+	console.log("Development");
 } else {
-    console.log('Production');
+	console.log("Production");
 }
-
 
 const Tab = createBottomTabNavigator();
 
@@ -28,171 +28,226 @@ export default class App extends Component {
 		super();
 		const emitter = ee();
 		this.state = {
-			globals : {
-				BASE_URL : "https://waterleakbackend.herokuapp.com",
-				emitter : emitter,
-				token : null
+			globals: {
+				BASE_URL: "https://waterleakbackend.herokuapp.com",
+				emitter: emitter,
+				token: null,
 			},
-			logged_in : false
-		}
-
+			logged_in: false,
+		};
 	}
 
-    
-    componentWillUnmount() {
-        
-	}
-	
+	componentWillUnmount() {}
+
+	// registerForPushNotificationsAsync = async () => {
+	// 	const { status } = await Permissions.askAsync(
+	// 		Permissions.NOTIFICATIONS
+	// 	);
+
+	// 	// Stop here if the user did not grant permissions
+	// 	if (status !== "granted") {
+	// 		alert("No notification permissions!");
+	// 		return;
+	// 	} else {
+	// 		console.log("Notifications are allowed");
+	// 	}
+
+	// 	// Get the token that identifies this device
+	// 	const token = await Notifications.getExpoPushTokenAsync();
+	// 	console.log("My token: " + token);
+
+	// 	if (Platform.OS === "android") {
+	// 		console.log("Android");
+	// 		Notifications.createChannelAndroidAsync("chat-messages", {
+	// 			name: "Chat messages",
+	// 			sound: true,
+	// 			priority: "high",
+	// 		});
+	// 	} else {
+	// 		console.log("IOS");
+	// 	}
+	// };
 	registerForPushNotificationsAsync = async () => {
-        const { status } = await Permissions.askAsync(
-            Permissions.NOTIFICATIONS
-        );
+		const { status: existingStatus } = await Permissions.getAsync(
+			Permissions.NOTIFICATIONS
+		);
+		let finalStatus = existingStatus;
+		if (existingStatus !== "granted") {
+			const { status } = await Permissions.askAsync(
+				Permissions.NOTIFICATIONS
+			);
+			finalStatus = status;
+		}
+		if (finalStatus !== "granted") {
+			alert("Failed to get push token for push notification!");
+			return;
+		}
+		const token = (await Notifications.getExpoPushTokenAsync()).data;
+		console.log(token);
+		this.setState({ expoPushToken: token });
 
+		if (Platform.OS === "android") {
+			Notifications.setNotificationChannelAsync("default", {
+				name: "default",
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: "#FF231F7C",
+			});
+		}
+	};
 
-        // Stop here if the user did not grant permissions
-        if (status !== "granted") {
-            alert("No notification permissions!");
-            return;
-        } else {
-            console.log("Notifications are allowed");
-        }
+	_handleNotificationInApp = async () => {
+		console.log("Handle Notification in App.");
+	};
+	async componentDidMount() {
+		if (Device.isDevice) {
+			console.log("Real Device");
+			this.registerForPushNotificationsAsync();
+			// Notifications.addListener = this._handleNotificationInApp;
 
-        // Get the token that identifies this device
-        const token = await Notifications.getExpoPushTokenAsync();
-        console.log("My token: " + token);
-
-        if (Platform.OS === "android") {
-            console.log("Android");
-            Notifications.createChannelAndroidAsync("chat-messages", {
-                name: "Chat messages",
-                sound: true,
-                priority: "high"
-            });
-        } else {
-            console.log("IOS");
-        }
-
-    };
-
-
-    async componentDidMount() {
-		if (Device.isDevice){
-			console.log("Real Device")
-			this.registerForPushNotificationsAsync()
+			// Notifications.addNotificationResponseReceivedListener = this._handleNotificationResponse;
 		}
 		// Check if auth-token is saved in AsyncStorage
 		try {
-			const value = await AsyncStorage.getItem('@authentication_save:auth_token');
+			const value = await AsyncStorage.getItem(
+				"@authentication_save:auth_token"
+			);
 			if (value !== null) {
 				// We have data!!
-				console.log("Read from Data" , value);
-				
+				console.log("Read from Data", value);
+
 				try {
-					await AsyncStorage.setItem('@authentication_save:verified_auth_token', value);
-					
+					await AsyncStorage.setItem(
+						"@authentication_save:verified_auth_token",
+						value
+					);
 				} catch (error) {
 					// Error saving data
-					console.log('Error saving data')
+					console.log("Error saving data");
 				}
 
-				this.setState({logged_in : true, token : value})
-				this.state.globals.emitter.emit("checked_token", value)
-				
+				this.setState({ logged_in: true, token: value });
+				this.state.globals.emitter.emit("checked_token", value);
 			}
-		  } catch (error) {
-			console.log("error reading data", error)
+		} catch (error) {
+			console.log("error reading data", error);
 		}
 
-		this.state.globals.emitter.on('logged_in', listener = async (json) => {
-			// … react to 'test' event
-			console.log("Logged In!", json)
-			console.log("Auth server token: " , json.token)
-		
-			
-			
-			
+		this.state.globals.emitter.on(
+			"logged_in",
+			(listener = async (json) => {
+				// … react to 'test' event
+				console.log("Logged In!", json);
+				console.log("Auth server token: ", json.token);
 
-			try {
-				await AsyncStorage.setItem('@authentication_save:auth_token', json.token);
-				await AsyncStorage.setItem('@authentication_save:verified_auth_token', json.token);
-				
-			} catch (error) {
-				// Error saving data
-				console.log('Error saving data')
-			}
-			this.setState({logged_in : true, token : json.token})
-			this.state.globals.emitter.emit("checked_token", json.token)
+				try {
+					await AsyncStorage.setItem(
+						"@authentication_save:auth_token",
+						json.token
+					);
+					await AsyncStorage.setItem(
+						"@authentication_save:verified_auth_token",
+						json.token
+					);
+				} catch (error) {
+					// Error saving data
+					console.log("Error saving data");
+				}
+				this.setState({ logged_in: true, token: json.token });
+				this.state.globals.emitter.emit("checked_token", json.token);
+			})
+		);
+		this.state.globals.emitter.on(
+			"logged_out",
+			(listener = async (json) => {
+				console.log("Logging Out recieved");
 
-		});
-		this.state.globals.emitter.on('logged_out', listener =  async (json) => {
-			console.log("Logging Out recieved")
-			
-
-			try {
-				await AsyncStorage.removeItem('@authentication_save:auth_token');
-				await AsyncStorage.removeItem('@authentication_save:verified_auth_token');
-				console.log("removed")
-			} catch (error) {
-				// Error saving data
-				console.log('Error deleting data')
-			}
-			this.state.globals.emitter.emit('logged_out_finished')
-			this.setState({logged_in : false, token : null})
-			
-		});
+				try {
+					await AsyncStorage.removeItem(
+						"@authentication_save:auth_token"
+					);
+					await AsyncStorage.removeItem(
+						"@authentication_save:verified_auth_token"
+					);
+					console.log("removed");
+				} catch (error) {
+					// Error saving data
+					console.log("Error deleting data");
+				}
+				this.state.globals.emitter.emit("logged_out_finished");
+				this.setState({ logged_in: false, token: null });
+			})
+		);
 	}
-	
-    render() {
-        let state = this.state;
+
+	render() {
+		let state = this.state;
 		// console.log("Logged in status: " , this.state.logged_in)
 		// console.log("app rerendered", this.state)
-		const submitGlobals = { globals: { emitter : this.state.globals.emitter, BASE_URL: this.state.globals.BASE_URL, token : this.state.token} };
+		const submitGlobals = {
+			globals: {
+				emitter: this.state.globals.emitter,
+				BASE_URL: this.state.globals.BASE_URL,
+				token: this.state.token,
+			},
+		};
 		// console.log('Submit Globals', submitGlobals.globals)
-        return (
-				<NavigationContainer>
-					<Tab.Navigator screenOptions={({ route }) => ({
-                        tabBarIcon: ({ focused, color, size }) => {
-                            let iconName;
+		return (
+			<NavigationContainer>
+				<Tab.Navigator
+					screenOptions={({ route }) => ({
+						tabBarIcon: ({ focused, color, size }) => {
+							let iconName;
 
-                            if (route.name === "Home") {
-                                iconName = focused
-                                    ? "ios-information-circle"
-                                    : "ios-information-circle-outline";
-                            }  else if (route.name === "Login") {
+							if (route.name === "Home") {
+								iconName = focused
+									? "ios-information-circle"
+									: "ios-information-circle-outline";
+							} else if (route.name === "Login") {
 								iconName = focused ? "md-person" : "md-person";
 							} else if (route.name === "Settings") {
-                                iconName = focused
-                                    ? "ios-list-box"
-                                    : "ios-list";
-                            } 
+								iconName = focused
+									? "ios-list-box"
+									: "ios-list";
+							}
 
-                            // You can return any component that you like here!
-                            return (
-                                <Ionicons
-                                    name={iconName}
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        }
-                    })}
-                    tabBarOptions={{
-                        activeTintColor: "tomato",
-                        inactiveTintColor: "gray"
-                    }}>
-							
-						
-						{ this.state.logged_in ? <React.Fragment>
-							<Tab.Screen name="Home" initialParams={{ globals: this.state.globals }} component={HomeScreen} />
-						<Tab.Screen name="Settings" initialParams={{ globals: this.state.globals }} component={SettingsScreen} />
-						
-						</React.Fragment> :  <Tab.Screen name="Login" initialParams={submitGlobals} component={LoginScreen} />}
-					
-					</Tab.Navigator>
-				</NavigationContainer>
-            
-        );
-    }
+							// You can return any component that you like here!
+							return (
+								<Ionicons
+									name={iconName}
+									size={size}
+									color={color}
+								/>
+							);
+						},
+					})}
+					tabBarOptions={{
+						activeTintColor: "tomato",
+						inactiveTintColor: "gray",
+					}}
+				>
+					{this.state.logged_in ? (
+						<React.Fragment>
+							<Tab.Screen
+								name="Home"
+								initialParams={{ globals: this.state.globals }}
+								component={HomeScreen}
+							/>
+							<Tab.Screen
+								name="Settings"
+								initialParams={{ globals: this.state.globals }}
+								component={SettingsScreen}
+							/>
+						</React.Fragment>
+					) : (
+						<Tab.Screen
+							name="Login"
+							initialParams={submitGlobals}
+							component={LoginScreen}
+						/>
+					)}
+				</Tab.Navigator>
+			</NavigationContainer>
+		);
+	}
 }
-
-
