4fb5a4ce5e55bfb6f7f2ae9dfa4d17e8fff20d81
leomet07 <leomet07@gmail.com>
2020-12-17 22:16:20 -0500

    added websocket to same server as express.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const dotenv = require("dotenv").config();
 
-
 const app = express();
 
+var http = require("http").createServer(app);
+
+const io = require("socket.io")(http, {
+	cors: {
+		origin: "*",
+	},
+});
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("tiny"))
+app.use(morgan("tiny"));
 
 // Only redirect to SSL if developer allows and states that machine running this has SSL to prevent crashes on computers without SSL
 if (process.env.SSL == "true") {
@@ -29,29 +35,37 @@ if (process.env.SSL == "true") {
 // import Routes
 const apiRouter = require("./routes/api").router;
 
-
 const db_str = process.env.DB_CONNECT;
 console.log(db_str);
 mongoose.connect(
-	db_str, {
+	db_str,
+	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: false,
-		useCreateIndex: true
+		useCreateIndex: true,
 	},
 	() => {
 		console.log("connected to db!");
 	}
 );
 
-
 //Routes Middleware
 app.use("/api/", apiRouter);
-app.get("/" , function( req, res) {
-	res.send("Hello World to Water Leak detector")
-})
+app.get("/", function (req, res) {
+	res.send("Hello World to Water Leak detector");
+});
 
-const port = process.env.PORT || 4201
-app.listen(port, () => {
-	console.log("Sever is up and running at http://127.0.0.1:" + port);
+// const port = process.env.PORT || 4201;
+// app.listen(port, () => {
+// 	console.log("Sever is up and running at http://127.0.0.1:" + port);
+// });
+http.listen(process.env.PORT || 3000, function () {
+	var host = http.address().address;
+	var port = http.address().port;
+	console.log("App listening at http://%s:%s", host, port);
+});
+
+io.on("connection", (client) => {
+	console.log("client connected");
 });
